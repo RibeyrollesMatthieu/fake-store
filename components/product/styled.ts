5ff76265eba3e89c1870dee28a29c2f35852afb1
Matthieu <ribeyrolles.matthieu@gmail.com>
2022-02-08 16:25:03 +0100

    animation on add to cart button on product cards

--- components/product/styled.ts
+++ components/product/styled.ts
@@ -31,4 +31,15 @@ export const ProductRating = styled.div`
 export const AddToCartButton = styled.button`
   width: 100%;
   cursor: pointer;
+  border: none;
+  background: linear-gradient(to right, #000 50%, #5c5c5c 50%);
+  background-size: 200%;
+  background-position: right;
+  padding: .5rem 1rem;
+  color: #fff;
+  transition: background .25s;
+
+  &:hover, &:focus {
+    background-position: left;
+  }
 `;
