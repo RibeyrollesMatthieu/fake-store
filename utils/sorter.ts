53cbe7fd0a07a86791bc686c242138c802f33ad5
Matthieu <ribeyrolles.matthieu@gmail.com>
2022-02-11 16:28:26 +0100

    added rating sorting

--- utils/sorter.ts
+++ utils/sorter.ts
@@ -1,11 +1,17 @@
 import { I_productType } from "../redux/app/types";
 
 export enum Sorters {
-  PRICE_ASC = "€ -> €€€", 
-  PRICE_DESC = "€€€ -> €",
+  PRICE_ASC = "Price: ascending", 
+  PRICE_DESC = "Price: descending",
 
-  NAME_ASC = "A -> Z",
-  NAME_DESC = "Z --> A",
+  NAME_ASC = "Name: ascending",
+  NAME_DESC = "Name: descending",
+
+  RATING_ASC = "Rating: worst to greatest",
+  RATING_DESC = "Rating: greatest to worst",
+
+  NUMBER_RATING_ASC = "Rating: few people to lots of people",
+  NUMBER_RATING_DESC = "Rating: lots of people to few people"
 }
 
 export const sortByPrice = (products: I_productType[], sorters: Sorters) => {
@@ -21,6 +27,18 @@ export const sortByPrice = (products: I_productType[], sorters: Sorters) => {
 
     case Sorters.NAME_DESC:
       return products.sort((productA, productB) => productA.title < productB.title ? 1 : -1 );
+
+    case Sorters.RATING_ASC:
+      return products.sort((productA, productB) => productA.rating.rate - productB.rating.rate);
+
+    case Sorters.RATING_DESC:
+      return products.sort((productA, productB) => productB.rating.rate - productA.rating.rate);
+
+    case Sorters.NUMBER_RATING_ASC:
+      return products.sort((productA, productB) => productA.rating.count - productB.rating.count);
+
+    case Sorters.NUMBER_RATING_DESC:
+      return products.sort((productA, productB) => productB.rating.count - productA.rating.count);
 
     default:
       return products.sort((productA, productB) => productA.price - productB.price);
