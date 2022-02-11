7c33e67e1e72d913c62bac9c724bde21892ba3ad
Matthieu <ribeyrolles.matthieu@gmail.com>
2022-02-11 19:15:01 +0100

    styling the sorter select

--- components/sorter-select/styled.ts
+++ components/sorter-select/styled.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
 export const SelectWrapper = styled.select`
+  margin: 3rem 1rem 1.5rem auto;
   width: fit-content;
+  background: #fff;
+  border: none;
+  padding: .25rem;
+  border-radius: 99vh;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
 `;
